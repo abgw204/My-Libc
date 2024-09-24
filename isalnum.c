5cb31d8f429de87631ae7d8d97163227fc402e80
gabriel <elementalgames77@gmail.com>
2024-09-24 11:19:53 -0300

    changes

--- isalnum.c
+++ isalnum.c
@@ -1,23 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   isalnum.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gada-sil <gada-sil@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/23 15:34:05 by gada-sil          #+#    #+#             */
-/*   Updated: 2024/09/24 11:09:24 by gada-sil         ###   ########.fr       */
+/*   Created: 2024/09/24 11:05:40 by gada-sil          #+#    #+#             */
+/*   Updated: 2024/09/24 11:19:22 by gada-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <string.h>
-# include <stdlib.h>
-
-size_t	ft_strlen(const char *str);
-int	ft_isdigit(int c);
-int	ft_isalpha(int c);
-
-#endif
+int	ft_isalnum(int c)
+{
+	if (ft_isdigit(c) || ft_isalpha(c))
+		return (1);
+	return (0);
+}
+/*#include <stdio.h>
+int main()
+{
+	printf("%d", ft_isalnum('a'));
+}*/
