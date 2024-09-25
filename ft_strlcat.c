861ca8a6e9b91fa8ed4b42d3dcb0c402e7fe28e7
gabriel <elementalgames77@gmail.com>
2024-09-25 16:52:17 -0300

    ft_changes

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,13 +6,13 @@
 /*   By: gada-sil <gada-sil@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 14:51:42 by gada-sil          #+#    #+#             */
-/*   Updated: 2024/09/25 16:01:26 by gada-sil         ###   ########.fr       */
+/*   Updated: 2024/09/25 16:46:56 by gada-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t    ft_strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 	size_t	j;
@@ -21,9 +21,11 @@ size_t    ft_strlcat(char *dst, const char *src, size_t size)
 	i = 0;
 	j = 0;
 	dst_len = ft_strlen(dst) + ft_strlen(src);
+	if (size == 0)
+		return (ft_strlen(src));
 	while (dst[i] != '\0')
 		i++;
-	while (src[j] != '\0' && j < size)
+	while (src[j] != '\0' && (i + j) < size)
 	{
 		dst[i] = src[j];
 		j++;
@@ -37,5 +39,5 @@ int    main(void)
 {
 	char src[] = "good morning";
 	char dest[] = "hello, ";
-	printf("%zu\n%s", ft_strlcat(dest, src, 15), dest);
+	printf("%zu\n%s", ft_strlcat(dest, src, 25), dest);
 }*/
