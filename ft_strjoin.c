f769c8107fb0324e206795d8e446edef12594b2d
gabriel <elementalgames77@gmail.com>
2024-09-26 14:59:38 -0300

    ft_changes

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,31 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gada-sil <gada-sil@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/25 16:08:23 by gada-sil          #+#    #+#             */
-/*   Updated: 2024/09/26 14:54:13 by gada-sil         ###   ########.fr       */
+/*   Created: 2024/09/26 14:20:14 by gada-sil          #+#    #+#             */
+/*   Updated: 2024/09/26 14:59:13 by gada-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	i;
+	size_t	s1_len;
+	size_t	s2_len;
 	char	*ptr;
+	size_t	i;
 
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
 	i = 0;
-	ptr = (char *)malloc(len + 1);
+	ptr = malloc(s1_len + s2_len + 1);
 	if (!ptr)
 		return (NULL);
-	while (i < len)
+	while (*s1)
 	{
-		ptr[i] = s[start];
-		i++;
-		start++;
+		ptr[i++] = *s1;
+		s1++;
+	}
+	while (*s2)
+	{
+		ptr[i++] = *s2;
+		s2++;
 	}
 	ptr[i] = '\0';
 	return (ptr);
@@ -33,6 +41,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 /*#include <stdio.h>
 int	main()
 {
-	char s[] = "this is a test";
-	printf("%s", ft_substr(s, 10, 4));
+	char	s1[] = "hello,";
+	char	s2[] = " how are you?";
+	printf("%s", ft_strjoin(s1, s2));
 }*/
