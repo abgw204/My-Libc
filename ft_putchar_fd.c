a4d106db16e7fd617b33f58f5ca1d9ebe7d0e09b
abgw204 <elementalgames77@gmail.com>
2024-09-28 18:07:31 -0300

    putstr and putchar fd's

---
+++ ft_putchar_fd.c
@@ -0,0 +1,9 @@
+#include "libft.h"
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+/*int	main()
+{
+	ft_putchar_fd('4', 1);
+}*/
---
+++ ft_putstr_fd.c
@@ -0,0 +1,12 @@
+#include "libft.h"
+void	ft_putstr_fd(char *s, int fd)
+{
+	while (*s != '\0')
+		write(fd, &*s++, 1);
+}
+/*#include <stdio.h>
+int	main()
+{
+	ft_putstr_fd("oiieeee", 1);
+	return (0);
+}*/
