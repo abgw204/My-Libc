cf617a3cf8b13fbebb2be1dfa231afc07600c7b9
abgw204 <elementalgames77@gmail.com>
2024-09-28 22:46:52 -0300

    fucking split

--- ft_split.c
+++ ft_split.c
@@ -1,3 +1,20 @@
+char	*str_malloc(char const *s, char c)
+{
+	int	i;
+	size_t	len;
+	size_t	n_of_arrays;
+
+	i = -1;
+	len = 0;
+	n_of_arrays = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			n_of_arrays++;
+
+	}
+}
 char	**ft_split(char const *s, char c)
 {
-		
+	
+}
