96e025d46db9953aee6518e992d1f0e1a03f4d11
gabriel <elementalgames77@gmail.com>
2024-10-01 17:04:00 -0300

    ft_changes

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -21,25 +21,30 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	ptr1 = (unsigned const char *)s1;
 	ptr2 = (unsigned const char *)s2;
 	i = 0;
-
-	while (ptr1[i] != '\0' && ptr2[i] != '\0' && i < n)
+	if (n == 0)
+		return (0);
+	while (i < n)
 	{
 		if (ptr1[i] != ptr2[i])
 			return (ptr1[i] - ptr2[i]);
 		i++;
 	}
-	return (ptr1[i] - ptr2[i]);
+	return (0);
 }
 /*#include <stdio.h>
 int	main()
 {
-	int	s1[3] = {1, 2, 3};
-	char	s2[] = "i'm a fucking idiot";
+	//int	s1[3] = {1, 2, 3};
+	//char	s2[] = "i'm a fucking idiot";
 
-	int	s3[3] = {1, 2, 3};
+	//int	s3[3] = {1, 2, 3};
 	//char	s3[] = "hello my name is kyle";
-	char	s4[] = "i'm a fucking idiot";
+	//char	s4[] = "i'm a fucking idiot";
 
-	printf("%d\n", ft_memcmp(s1, s2, 3));
-	printf("%d\n", memcmp(s3, s4, 3));
+	//printf("%d\n", ft_memcmp(s1, s2, 3));
+	//printf("%d\n", memcmp(s3, s4, 3));
+	char s[] = {-128, 0, 127, 0};
+	char s2[] = {0, 0, 127, 0};
+	//printf("%d", memcmp(s, s2, 0));
+	printf("%d", ft_memcmp(s, s2, 0));
 }*/
