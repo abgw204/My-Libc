e54d69c70e4a36ae43cb51cfd29e15f76ec69ee4
abgw204 <elementalgames77@gmail.com>
2024-10-02 20:15:46 -0300

    fucking striteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -10,7 +10,30 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <ctype.h>
+#include <stdio.h>
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-	s++;
+	unsigned int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		f(i, &s[i]);
+		i++;
+	}
 }
+/*void	toUPPER(unsigned int i, char *s)
+{
+	unsigned int a;
+	a = i;
+	a = a + 1;
+	*s = toupper(*s);
+}
+int	main()
+{
+	char s[] = "ooooopp";
+	ft_striteri(s, toUPPER);
+	printf("%s", s);
+	return 0;
+}*/
